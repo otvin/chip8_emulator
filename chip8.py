be8f137b714e57ded4848d713e83ce0c9dd97032
otvin <michael_fredericks@hotmail.com>
2022-12-18 18:12:43 -0500

    Added quick sound

--- chip8.py
+++ chip8.py
@@ -1,6 +1,20 @@
 import pygame
+from array import array
 
 SCALE_FACTOR = 16
+
+
+def build_pygame_sound_samples():
+    # modified from: https://gist.github.com/ohsqueezy/6540433
+    period = int(round(pygame.mixer.get_init()[0] / 440))
+    samples = array("h", [0] * period)
+    amplitude = 2 ** (abs(pygame.mixer.get_init()[1]) - 1) - 1
+    for time in range(period):
+        if time < period / 2:
+            samples[time] = amplitude
+        else:
+            samples[time] = -amplitude
+    return samples
 
 class C8Screen:
     def __init__(self):
@@ -27,11 +41,14 @@ class C8Screen:
                         window.set_at((startx + i, starty + j), color)
 
 def main():
-
+    pygame.mixer.pre_init(44100, -16, 1, 1024)
     pygame.init()
     window = pygame.display.set_mode((64 * SCALE_FACTOR, 32 * SCALE_FACTOR))
     pygame.display.set_caption("Fred's CHIP-8 Emulator")
     window.fill(0)
+
+    beep = pygame.mixer.Sound(build_pygame_sound_samples())
+    beep.set_volume(0.1)
 
     run = True
     myscreen = C8Screen()
@@ -50,8 +67,13 @@ def main():
                 px += 1
                 if px % 2 == 0:
                     py += 1
+            elif event.type == pygame.KEYDOWN:
+                beep.play(-1)
+            elif event.type == pygame.KEYUP:
+                beep.stop()
         pygame.display.flip()
 
+    pygame.midi.quit()
 
 if __name__ == "__main__":
     # call the main function
